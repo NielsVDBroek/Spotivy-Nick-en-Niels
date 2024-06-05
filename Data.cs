1e5ab9aedc4e7b4b9478c032b44b118dc62f3394
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2024-06-05 12:46:44 +0200

    Nummer aanmaken start

--- Data.cs
+++ Data.cs
@@ -1,21 +1,21 @@
-﻿namespace Spotivy_Nick_en_Niels
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
 
+namespace Spotivy_Nick_en_Niels
+{
+    internal class Data
+    {
+        public static void AddStandardData()
+        {
             Artist Artist1 = new Artist("Test Artist 1", "Artist1Password");
             Artist Artist2 = new Artist("Test Artist 1", "Artist1Password");
             Song Song1 = new Song("Test Nummer 1", Artist1, "Test Text 1");
             Song Song2 = new Song("Test Nummer 2", Artist2, "Test Text 2");
-            //Data.AddStandardData();
-
-            Console.WriteLine("Data toegevoegd!");
-            Console.WriteLine(DateTime.Now);
-            Song1.ShowInfo();
-            Song1.PlaySong();
-            Song1.ShowInfo();
         }
     }
 }
--- Song.cs
+++ Song.cs
@@ -11,22 +11,37 @@ namespace Spotivy_Nick_en_Niels
         private string Name { get; }
         private Artist Artist { get; }
         private string Text { get; }
-        private string Date { get; }
-        private int TotalPlays { get; }
+        public DateTime Date { get; private set; }
+        private int TotalPlays { get; set; }
         public Album Album { get; private set; }
 
-        public Song(string name, Artist artist, string text, string date) 
+        public Song(string name, Artist artist, string text) 
         {
             Name = name;
             Artist = artist;
             Text = text;
-            Date = date;
+            Date = DateTime.Now;
             TotalPlays = 0;
         }
 
-        public void PlaySong() { }
+        public void SetAlbum(Album album)
+        {
+            Album = album;
+        }
+
+        public void PlaySong() 
+        {
+            TotalPlays++;
+        }
         public void PauseSong() { }
-        public void ShowInfo() { }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Artist: {Artist}");
+            Console.WriteLine($"Text: {Text}");
+            Console.WriteLine($"Date: {Date.ToString("dd/MM/yyyy")}");
+            Console.WriteLine($"TotalPlays: {TotalPlays}");
+        }
         public void AddToList() { }
     }
 }
